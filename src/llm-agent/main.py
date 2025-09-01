9763dca9deb2b8ccf9d07c60714f51a72256ab3a
yokotsuji <146211582+yokotsuji@users.noreply.github.com>
2025-09-01 18:06:10 +0900

    Create main.py

---
+++ src/llm-agent/main.py
@@ -0,0 +1,78 @@
+from Agent import Agent
+from Prompt import generate_prompt, taskorganize_prompt, selectiotservice_prompt, executetask_prompt, conbine_prompt, responsetouser_prompt, test_prompt
+from Memory import save_to_database
+from Tool import searchIoTService_Tool, getEvent_Tool, notifyEvent_Tool
+import sys
+from colorama import Fore, Back, Style
+import time
+import openpyxl
+from config import OPENAI_API_KEY_1
+from GeminiAgent import GeminiAgent
+from query import atomic_queries, complex_queries, ambiguous_queries
+
+def main():
+    start = time.time()
+#    run_multiple_queries_and_save_times(atomic_queries, "atomic_")
+    run_multiple_queries_and_save_times(complex_queries, "complex_")
+#    run_multiple_queries_and_save_times(ambiguous_queries, "ambiguous_")
+        end = time.time()
+        print(f"response time: {end - start}")
+        break
+
+
+def run_agent_once(user_input):
+    start = time.time()
+
+    test_agent= GeminiAgent([getEvent_Tool, notifyEvent_Tool], test_prompt)
+    print(Fore.GREEN + "AI Assistant is ready. Type 'exit' to quit." + Style.RESET_ALL)
+
+    response = test_agent.respond(user_input)
+    print(response)
+
+    end = time.time()
+    elapsed = end - start
+    print(f"response time: {elapsed:.2f} seconds")
+    return response, test_agent.tool_use
+
+
+def run_agent_once2(user_input):
+    start = time.time()
+
+#    test_agent = Agent([getEvent_Tool, notifyEvent_Tool], test_prompt, OPENAI_API_KEY_1)
+    test_agent= GeminiAgent([getEvent_Tool, notifyEvent_Tool], test_prompt)
+    print(Fore.GREEN + "AI Assistant is ready. Type 'exit' to quit." + Style.RESET_ALL)
+
+    response = test_agent.respond(user_input)
+    print(response)
+    while user_input != "exit":
+        user_input = input("You: ")
+        print(test_agent.respond(user_input))
+    end = time.time()
+    elapsed = end - start
+    print(f"response time: {elapsed:.2f} seconds")
+    return response, test_agent.tool_use
+
+def run_multiple_queries_and_save_times(queries, file):
+    """
+    クエリごとに10回ずつエージェントを実行し、応答時間をExcelに保存する
+    :param queries: ユーザクエリのリスト
+    :param base_filename: 保存先Excelファイル名
+    """
+    base_filename="gpt_4o.xlsx"
+    filename = file + base_filename
+    print(filename)
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "Agent Responses"
+    ws.append(["クエリ", "応答内容", "利用したツール"])
+
+    for query in queries:
+#            print(f"\n=== クエリ「{query}」の実行 {i+1} 回目 ===")
+        result, tool_use = run_agent_once(query)  # run_agent_onceは (str, float) を返すと仮定
+        ws.append([query, result, str(tool_use)])
+
+    wb.save(filename)
+    print(f"\n✅ 結果を「{filename}」に保存しました。")
+
+if __name__ == "__main__":
+    main()
